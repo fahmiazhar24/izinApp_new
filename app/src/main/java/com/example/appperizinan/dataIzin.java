42899152379471304e8cfd96236966eac7528848
OA7019 <muhammad.taufik.ft17@mail.umy.ac.id>
2019-07-22 15:13:07 +0700

    edit dataIzin

--- app/src/main/java/com/example/appperizinan/dataIzin.java
+++ app/src/main/java/com/example/appperizinan/dataIzin.java
@@ -4,6 +4,10 @@ package com.example.appperizinan;
 public class dataIzin {
     private String nama;
     private String nim;
+    private String kelas;
+    private String matkul;
+    private String jenisIzin;
+    private String alasan;
 
     public String getNama() {
         return nama;
@@ -13,12 +17,34 @@ public class dataIzin {
         return nim;
     }
 
+    public String getKelas() {
+        return kelas;
+    }
+
+    public String getMatkul() {
+        return matkul;
+    }
+
+    public String getJenisIzin() {
+        return jenisIzin;
+    }
+
+    public String getAlasan() {
+        return alasan;
+    }
+
+
+
     public dataIzin(){
 
     }
 
-    public dataIzin(String nama, String nim){
+    public dataIzin(String nama, String nim, String kelas, String matkul, String jenisIzin, String alasan){
         this.nama = nama;
         this.nim = nim;
+        this.kelas = kelas;
+        this.matkul = matkul;
+        this.jenisIzin = jenisIzin;
+        this.alasan = alasan;
     }
 }

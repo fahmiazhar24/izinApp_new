4acbbfb757e775fb30394589b6e668ba475ed85b
OA7019 <muhammad.taufik.ft17@mail.umy.ac.id>
2019-07-22 14:52:39 +0700

    push Admin

--- app/src/main/java/com/example/appperizinan/adminPage.java
+++ app/src/main/java/com/example/appperizinan/adminPage.java
@@ -8,14 +8,23 @@ import android.widget.Button;
 
 public class adminPage extends AppCompatActivity {
 
-    Button logout;
+    Button logout, daftIzin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
 
+        daftIzin = findViewById(R.id.dftr_izin);
         logout = findViewById(R.id.keluar);
+
+        daftIzin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent daftar = new Intent(adminPage.this, daftarIzinAdmin.class);
+                startActivity(daftar);
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
